78be6fb46d108fe6f9a6873b2b3a640da96d0e8a
priyankaj04 <priyankaj2002o4@gmail.com>
2023-08-27 21:07:53 +0530

    about us component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    ContactusComponent
   ],
   imports: [
     BrowserModule,
